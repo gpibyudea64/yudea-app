9c17a22267215e048e4d052e1bb1de1a77e8ecc7
GMarcell <grand1310marcell@gmail.com>
2026-06-02 22:59:36 +0700

    debug: adjust proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,20 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
   const { pathname } = req.nextUrl;
 
-  // protect dashboard
   if (!isLoggedIn && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/public/login", req.url));
   }
 
-  // prevent logged-in users from visiting login
   if (isLoggedIn && pathname.startsWith("/public/login")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
-  // IMPORTANT: explicitly continue
   return;
 });
 
