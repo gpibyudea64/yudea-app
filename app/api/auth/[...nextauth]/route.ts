983f0097806ea2b4ad62c01c106a3acf3d786002
GMarcell <grand1310marcell@gmail.com>
2026-06-02 18:05:11 +0700

    fic: change auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,12 @@ import { handlers } from "@/auth";
 
 export const runtime = "nodejs";
 
-export async function GET(req: Request, ctx: any) {
+export async function GET(req: Request) {
   console.log("AUTH GET URL:", req.url);
-  return handlers.GET(req, ctx);
+  return handlers.GET(req);
 }
 
-export async function POST(req: Request, ctx: any) {
+export async function POST(req: Request) {
   console.log("AUTH POST URL:", req.url);
-  return handlers.POST(req, ctx);
+  return handlers.POST(req);
 }
