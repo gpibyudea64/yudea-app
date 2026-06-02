9a41027d869afab7b4092df383c118b41dbf96fc
GMarcell <grand1310marcell@gmail.com>
2026-06-02 23:03:18 +0700

    debug: adjust proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,7 @@
 import { auth } from "@/auth";
-import { NextResponse } from "next/server";
 
-export const runtime = "edge";
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const { pathname } = req.nextUrl;
-
-  if (!isLoggedIn && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/public/login", req.url));
-  }
-
-  if (isLoggedIn && pathname.startsWith("/public/login")) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  return;
-});
+export default auth;
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/public/login"],
+  matcher: ["/dashboard/:path*"],
 };
