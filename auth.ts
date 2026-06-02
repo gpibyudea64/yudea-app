e5379bdf3c6a1c439752a3f69a2fd24c1cd1bc0f
GMarcell <grand1310marcell@gmail.com>
2026-06-02 22:45:15 +0700

    debug: add console.log to authorize function

--- auth.ts
+++ auth.ts
@@ -14,13 +14,35 @@ const prisma = new PrismaClient({ adapter });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Credentials({
       credentials: {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
+      // async authorize(credentials) {
+      //   if (!credentials?.email || !credentials?.password) return null;
+
+      //   const user = await prisma.user.findUnique({
+      //     where: { email: credentials.email as string },
+      //   });
+
+      //   if (!user || !user.password) return null;
+
+      //   const isValid = await bcrypt.compare(
+      //     credentials.password as string,
+      //     user.password,
+      //   );
+
+      //   if (!isValid) return null;
+
+      //   return {
+      //     ...user,
+      //     role: normalizeAppRole(user.role),
+      //     regionId: user.regionId ?? undefined,
+      //   } as any;
+      // },
 
       async authorize(credentials) {
         console.log("AUTH credentials", credentials);
